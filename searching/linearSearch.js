82d349edee5e073711bf8fa78fecd2280a67d821
asifalam515 <asibulalam515@gmail.com>
2023-02-25 23:59:22 +0600

    code refactored

--- searching/linearSearch.js
+++ searching/linearSearch.js
@@ -1,10 +1,19 @@
-function linearSearch(arr,target){ //the function of linear search.where we use two parameters.arr will takes an array(through which the operation will be held) and a target (which is our targeted value)
-    for(let i=0;i<arr.length;i++){ //loop through the whole array
-        if(arr[i]===target){ //will check if the element is the  targeted element or not
-            return i; //if is it.then it will return the index of the element
+//the function of linear search.
+// where we use two parameters.
+// arr will takes an array(through which the operation will be held) 
+// and a target (which is our targeted value)
+function linearSearch(arr,target){ 
+    //loop through the whole array
+    for(let i=0;i<arr.length;i++){ 
+        //will check if the element is the  targeted element or not
+        if(arr[i]===target){ 
+            //if is it.then it will return the index of the element
+            return i; 
         }
     }
-    return -1;//if we don't find the targeted element even after checking through all elements,then we will return -1
+    //if we don't find the targeted element 
+    // even after checking through all elements,then we will return -1
+    return -1;
 
 }
 
